8eabcef1852cdb6239002ca93ff19d8b905497cd
Harvey Yerik <yerik05vh@gmail.com>
2023-03-07 11:53:28 -0500

    Dev: add assets

---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppTheme{
+  static const Color whiteSky = Color(0xffF6FAFF);
+  static final ThemeData themeConfig = ThemeData.light().copyWith(
+    scaffoldBackgroundColor: whiteSky
+  );
+}
