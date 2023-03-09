4bc58bcb1ac635d7121331591902c3effe63d259
Harvey Yerik <yerik05vh@gmail.com>
2023-03-09 10:00:27 -0500

    Dev: update data

--- lib/screens/add_habit_screen.dart
+++ lib/screens/add_habit_screen.dart
@@ -1,3 +1,4 @@
+import 'package:farm_habit/theme/app_theme.dart';
 import 'package:farm_habit/widgets/span_text.dart';
 import 'package:flutter/material.dart';
 
@@ -14,12 +15,25 @@ class AddHabitScreen extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           AppBarSample(buttonText: "Cancelar",title: "Agregar habito",),
+          const SizedBox(
+            width: double.infinity,
+            child: Divider(
+              color: AppTheme.black100,
+              thickness: 1,
+            ),
+          ),
           const SizedBox(height: 20.0,),
           TitleForm(title: "Nombre del habito",),
-          const SizedBox(height: 10.0,),
+         const SizedBox(height: 10.0,),
           TextFieldWidget(hintText: "Ingresa el nombre del habito"),
           const SizedBox(height: 5.0,),
-          SpanText(description: "Cuando elijas el nombre de tu hábito, asegúrate de que sea una acción clara en lugar de una lucha interna. Por ejemplo, en lugar de 'dejar de procrastinar', elige 'iniciar las tareas temprano'.")
+          SpanText(description: "Cuando elijas el nombre de tu hábito, asegúrate de que sea una acción clara en lugar de una lucha interna. Por ejemplo, en lugar de 'dejar de procrastinar', elige 'iniciar las tareas temprano'.") ,
+          const SizedBox(height: 20.0,),
+          TitleForm(title: "Icono del habito ",),
+         const SizedBox(height: 10.0,),
+          TextFieldWidget(hintText: "Ingresa el nombre del habito"),
+          const SizedBox(height: 5.0,),
+          SpanText(description: "Te recomendamos elegir un icono para tu hábito, de esta forma podrás filtrar y visualizar de manera más clara todos tus hábitos. ¡Anímate a personalizar tu lista de hábitos!") 
         ],
       )
     );
